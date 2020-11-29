91b078d8b628d1bb0a0c751c198bb9277a802376
franekl <73118808+franekl@users.noreply.github.com>
2020-11-29 19:06:51 +0100

    Adding Employee class

---
+++ Employee.java
@@ -0,0 +1,39 @@
+import java.io.FileNotFoundException;
+
+public class Employee {
+
+        private int ID;
+        private int pin;
+        private String type;
+
+        public void showMemberList() throws FileNotFoundException {
+            MemberList ml = new MemberList();
+            ml.scanFile();
+            ml.printMembers();
+        }
+
+        public int getID() {
+            return this.ID;
+        }
+
+        public void setID(int ID) {
+            this.ID = ID;
+        }
+
+        public int getPin() {
+            return this.pin;
+        }
+
+        public void setPin(int pin) {
+            this.pin = pin;
+        }
+
+        public String getType() {
+            return this.type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+    }
