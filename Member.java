cbfce18cdd7e8ed95a47e03e21f5be9ccae512fd
Sintry1 <75076281+Sintry1@users.noreply.github.com>
2020-11-30 22:43:04 +0100

    Update Member.java
    Added a method for the membership price and also altered the setID method slightly. Input welcome, not sure if it works as it is.

--- Member.java
+++ Member.java
@@ -1,7 +1,6 @@
 /* 
  
  This is Chiara's edited version of the Member class! I had to do some changes because I couldn't work with the previous version... :(
-
    What changed (29/11 @ 16:30) : 
    
       - added memDOB (date of birth);
@@ -16,103 +15,172 @@
    
       - format dates so they will be displayed nicely with only the info that we need ("dd-MM-yyyy")
       - perhaps add a toString method? where will it be used?
-
-
  */
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class Member {
 
-    private int memID;
-    private String memName;
-    private LocalDate memDOB;
-    private int memAge; 
-    private String address;
-    private String favDiscipline;
-
-    private LocalDate regDate;
-    private String memType;
-    private String ageRange;
-    private boolean memValidity = false;
-    private boolean activityLevel;
+   private int memID;
+   private String memName;
+   private LocalDate memDOB;
+   private int memAge; 
+   private String address;
+   private int membershipPrice = 500;
+   private String favDiscipline;
+   private int memPrice = 500;
+   private LocalDate regDate;
+   private String memType;
+   private String ageRange;
+   private boolean memValidity = false;
+   private boolean activityLevel;
+   
+   Random r = new Random();
+   
+   private double setMembershipPrice(){ // Unsure if this will work. Need some way to define the memType. Enum maybe? I just don't know how to do an Enum.
+      if (this.memAge < 18 && this.memType.equals("Active")){
+         this.memPrice = 1000;
+      }else if (this.memAge >= 18 && this.memType.equals("Active") && this.memAge < 60){
+         this.memPrice = 1600;
+      }else if (this.memAge >= 60 && this.memType.equals("Active")){
+         this.memPrice = 1200;
+      }else{
+         this.memPrice = memPrice;
+      }
+      return memPrice;
+   }
     
-    public Member(int ID, String name, LocalDate birth, String address, String disc, String membership, boolean level) {
+   public Member(int ID, String name, LocalDate birth, String address, String disc, String membership, boolean level) {
     
-       this.memID = ID;
-       this.memName = name;
-       this.memDOB = birth;
-       this.address = address;
-       this.favDiscipline = disc;
-       this.memType = membership;
-       this.activityLevel = level;
-       
-       this.regDate = LocalDate.now();
-       this.setAge();
-       this.setAgeRange();
+      this.memID = ID;
+      this.memName = name;
+      this.memDOB = birth;
+      this.address = address;
+      this.favDiscipline = disc;
+      this.memType = membership;
+      this.activityLevel = level;
+      this.regDate = LocalDate.now();
+      this.setAge();
+      this.setAgeRange();
+      setMembershipPrice();
+      setID(); //Not sure if this belongs here?
        
        //the validity will be set from the treasurer when they actually pay!!!
     
-    }
+   }
 
      
-    public void setID(int memID) { this.memID = memID; }
-    public int getID() { return this.memID; }
+   public void setID(int memID) { //This should work to set the memID to be a random number from 1-9999 I think? Call it in the constructor maybe?
+      memID = r.nextInt(9998) + 1;
+      this.memID = memID; 
+   }
+  
+   public int getID() { 
+      return this.memID; 
+   }
 
     
-    public void setName(String memName) { this.memName = memName; }
-    public String getName() { return this.memName; }
+   public void setName(String memName) { 
+      this.memName = memName; 
+   }
+   
+   public String getName() { 
+      return this.memName; 
+   }
     
 
-    public void setDOB(LocalDate memDOB) { this.memDOB = memDOB; } 
-    public LocalDate getDOB() { return this.memDOB; }
+   public void setDOB(LocalDate memDOB) { 
+      this.memDOB = memDOB; 
+   } 
+   
+   public LocalDate getDOB() { 
+      return this.memDOB; 
+   }
     
     
-    public void setAddress(String address) { this.address = address; }
-    public String getAddress() { return this.address; }
+   public void setAddress(String address) { 
+      this.address = address; 
+   }
+   
+   public String getAddress() { 
+      return this.address; 
+   }
     
     
-    public void setDiscipline(String favDiscipline) { this.favDiscipline = favDiscipline; }
-    public String getDiscipline() { return this.favDiscipline; }
+   public void setDiscipline(String favDiscipline) {
+      this.favDiscipline = favDiscipline; 
+   }
+   
+   public String getDiscipline() { 
+      return this.favDiscipline; 
+   }
     
     
-    public void setRegDate(LocalDate regDate) { this.regDate = regDate; }
-    public LocalDate getRegDate() { return this.regDate; }
+   public void setRegDate(LocalDate regDate) { 
+      this.regDate = regDate; 
+   }
+   
+   public LocalDate getRegDate() { 
+      return this.regDate; 
+   }
     
     
-    public void setType(String memType) { this.memType = memType; }
-    public String getType() { return this.memType; }
+   public void setType(String memType) { 
+      this.memType = memType; 
+   }
+   
+   public String getType() { 
+      return this.memType; 
+   }
     
     
     //calculateAge() is returning an ``int`` comparing the DOB and the current date
-    public int calculateAge(LocalDate DOB) {  return Period.between(DOB, LocalDate.now()).getYears();  }
+   public int calculateAge(LocalDate DOB) {  
+      return Period.between(DOB, LocalDate.now()).getYears();  
+   }
     
     
-    public void setAge() { this.memAge = calculateAge(this.memDOB); }
-    public int getAge() { return this.memAge; }
+   public void setAge() { 
+      this.memAge = calculateAge(this.memDOB); 
+   }
+   
+   public int getAge() { 
+      return this.memAge; 
+   }
     
     
 
-    public void setAgeRange() {
+   public void setAgeRange() {  
+      if ( this.getAge() < 18) ageRange = "Junior";
+      else ageRange = "Senior";
         
-        if ( this.getAge() < 19) ageRange = "Junior";
-        else ageRange = "Senior";
-        
-    }
+   }
     
-    public String getAgeRange() { return this.ageRange; }
+   public String getAgeRange() { 
+      return this.ageRange; }
 
 
-    public void setValidity(boolean memValidity) { this.memValidity = memValidity; }
-    public boolean getValidity() { return memValidity; }
+   public void setValidity(boolean memValidity) { 
+      this.memValidity = memValidity; 
+   }
+   
+   public boolean getValidity() { 
+      return memValidity; 
+   }
     
     
 
 
     //the ActivityLevel will be true when the membership is active and false when the membership is passive!
-    public void setActivityLevel(boolean activityLevel) { this.activityLevel = activityLevel; }
-    public boolean getActivityLevel() { return activityLevel; }
+   public void setActivityLevel(boolean activityLevel) { 
+      this.activityLevel = activityLevel; 
+   }
+   
+   public boolean getActivityLevel() { 
+      return activityLevel; 
+   }
     
 
 }
