df52f626e01bf01fbaf92308b12219c2ef655b37
MrBigRedButton <MrBigRedButton@users.noreply.github.com>
2020-11-30 17:59:25 +0100

    MemberList v0.2 (dupe recognition missing)

--- MemberList.java
+++ MemberList.java
@@ -2,11 +2,24 @@ import java.util.*;
 import java.io.*;
 
 public class MemberList{
+<<<<<<< Updated upstream
    private ArrayList<Member> members = new ArrayList<Member>();
    
    //Adds a new member
    public void addMember(int newID, String newName, int newAge, String newAddress, String newDiscipline, String newType, boolean newValidity, boolean newActivityLevel){
       members.add(new Member(newID, newName, newAge, newAddress, newDiscipline, newType, newValidity, newActivityLevel));
+=======
+
+   LocalDate date;
+
+   private ArrayList<Member> members = new ArrayList<Member>();
+   
+//   Member m = new Member();
+   
+   //Adds a new member
+   public void addMember(Member m){
+      members.add(m);
+>>>>>>> Stashed changes
    }
    
    //Deletes member based on ID
@@ -22,7 +35,11 @@ public class MemberList{
    public void scanFile() throws FileNotFoundException{
       Scanner scanFile = new Scanner(new File("Member List.txt"));
       while (scanFile.hasNextLine()){
+<<<<<<< Updated upstream
          addMember(scanFile.nextInt(), scanFile.next(), scanFile.nextInt(), scanFile.next(), scanFile.next(), scanFile.next(), scanFile.nextBoolean(), scanFile.nextBoolean());
+=======
+         addMember(new Member(scanFile.nextInt(), scanFile.next(), date.of(scanFile.nextInt(), scanFile.nextInt(), scanFile.nextInt()), scanFile.next(), scanFile.next(), scanFile.next(), scanFile.nextBoolean()));
+>>>>>>> Stashed changes
       }
    }
    
@@ -39,11 +56,11 @@ public class MemberList{
       try{
          try{
             for (int i = 0; i < (members.size() - 1); i++){
-               save.println(members.get(i).getID() + " " + members.get(i).getName() + " " + members.get(i).getAge() + " " + members.get(i).getAddress() + " " + members.get(i).getDiscipline() + " " + members.get(i).getType() + " " + members.get(i).getValidity() + " " + members.get(i).getActivityLevel());
+               save.println(members.get(i).getID() + " " + members.get(i).getName() + " " + members.get(i).getDOB().getYear() + " " + members.get(i).getDOB().getMonthValue() + " " + members.get(i).getDOB().getDayOfMonth() + " " + members.get(i).getAddress() + " " + members.get(i).getDiscipline() + " " + members.get(i).getType() + " " + members.get(i).getActivityLevel());
             }
-            save.print(members.get(members.size() - 1).getID() + " " + members.get(members.size() - 1).getName() + " " + members.get(members.size() - 1).getAge() + " " + members.get(members.size() - 1).getAddress() + " " + members.get(members.size() - 1).getDiscipline() + " " + members.get(members.size() - 1).getType() + " " + members.get(members.size() - 1).getValidity() + " " + members.get(members.size() - 1).getActivityLevel());
+            save.print(members.get(members.size() - 1).getID() + " " + members.get(members.size() - 1).getName() + " " + members.get(members.size() - 1).getDOB().getYear() + " " + members.get(members.size() - 1).getDOB().getMonthValue() + " " + members.get(members.size() - 1).getDOB().getDayOfMonth() + " " + members.get(members.size() - 1).getAddress() + " " + members.get(members.size() - 1).getDiscipline() + " " + members.get(members.size() - 1).getType() + " " + members.get(members.size() - 1).getActivityLevel());
          }catch (Exception e){
-            save.print(members.get(0).getID() + " " + members.get(0).getName() + " " + members.get(0).getAge() + " " + members.get(0).getAddress() + " " + members.get(0).getDiscipline() + " " + members.get(0).getType() + " " + members.get(0).getValidity() + " " + members.get(0).getActivityLevel());
+            save.print(members.get(0).getID() + " " + members.get(0).getName() + " " + members.get(0).getDOB().getYear() + " " + members.get(0).getDOB().getMonthValue() + " " + members.get(0).getDOB().getDayOfMonth() + " " + members.get(0).getAddress() + " " + members.get(0).getDiscipline() + " " + members.get(0).getType() + " " + members.get(0).getActivityLevel());
          }
       }catch (Exception e){}
    }
