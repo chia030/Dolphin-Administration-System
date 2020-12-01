8cf133d1dc2651c1fd1816de9ecbc435930054f2
MrBigRedButton <MrBigRedButton@users.noreply.github.com>
2020-12-01 11:39:52 +0100

    MemberList v0.4

--- MemberList.java
+++ MemberList.java
@@ -28,14 +28,14 @@ public class MemberList{
    public void scanFile() throws FileNotFoundException{
       Scanner scanFile = new Scanner(new File("Member List.txt"));
       while (scanFile.hasNextLine()){
-         addMember(new Member(scanFile.nextInt(), scanFile.next(), date.of(scanFile.nextInt(), scanFile.nextInt(), scanFile.nextInt()), scanFile.next(), scanFile.next(), scanFile.next(), scanFile.nextBoolean()));
+         addMember(new Member(scanFile.nextInt(), scanFile.next(), date.of(scanFile.nextInt(), scanFile.nextInt(), scanFile.nextInt()), scanFile.next(), scanFile.next(), scanFile.next(), scanFile.nextBoolean(), date.of(scanFile.nextInt(), scanFile.nextInt(), scanFile.nextInt()), scanFile.nextBoolean()));
       }
    }
    
    //Prints all contents of member list
    public void printMembers(){
       for (int i = 0; i < members.size(); i++){
-         System.out.println("ID: " + members.get(i).getID() + "\nName: " + members.get(i).getName() + "\nAge: " + members.get(i).getAge() + "\nAge group: " + members.get(i).getAgeRange() + "\nAddress: " + members.get(i).getAddress() + "\nDiscipline: " + members.get(i).getDiscipline() + "\nType: " + members.get(i).getType() + "\nValidity: " + members.get(i).getValidity() + "\nActivity: " + members.get(i).getActivityLevel() + "\n-----------------");
+         System.out.println("ID: " + members.get(i).getID() + "\nName: " + members.get(i).getName() + "\nAge: " + members.get(i).getAge() + "\nAge group: " + members.get(i).getAgeRange() + "\nAddress: " + members.get(i).getAddress() + "\nDiscipline: " + members.get(i).getDiscipline() + "\nType: " + members.get(i).getType() + "\nValidity: " + members.get(i).getValidity() + "\nActivity: " + members.get(i).getActivityLevel() + "\nRegistration date: " + members.get(i).getRegDate() + "\n-----------------");
       }
    }
    
@@ -45,11 +45,11 @@ public class MemberList{
       try{
          try{
             for (int i = 0; i < (members.size() - 1); i++){
-               save.println(members.get(i).getID() + " " + members.get(i).getName() + " " + members.get(i).getDOB().getYear() + " " + members.get(i).getDOB().getMonthValue() + " " + members.get(i).getDOB().getDayOfMonth() + " " + members.get(i).getAddress() + " " + members.get(i).getDiscipline() + " " + members.get(i).getType() + " " + members.get(i).getActivityLevel());
+               save.println(members.get(i).getID() + " " + members.get(i).getName() + " " + members.get(i).getDOB().getYear() + " " + members.get(i).getDOB().getMonthValue() + " " + members.get(i).getDOB().getDayOfMonth() + " " + members.get(i).getAddress() + " " + members.get(i).getDiscipline() + " " + members.get(i).getType() + " " + members.get(i).getActivityLevel() + " " + members.get(i).getRegDate().getYear() + " " + members.get(i).getRegDate().getMonthValue() + " " + members.get(i).getRegDate().getDayOfMonth() + " " + members.get(i).getValidity());
             }
-            save.print(members.get(members.size() - 1).getID() + " " + members.get(members.size() - 1).getName() + " " + members.get(members.size() - 1).getDOB().getYear() + " " + members.get(members.size() - 1).getDOB().getMonthValue() + " " + members.get(members.size() - 1).getDOB().getDayOfMonth() + " " + members.get(members.size() - 1).getAddress() + " " + members.get(members.size() - 1).getDiscipline() + " " + members.get(members.size() - 1).getType() + " " + members.get(members.size() - 1).getActivityLevel());
+            save.print(members.get(members.size() - 1).getID() + " " + members.get(members.size() - 1).getName() + " " + members.get(members.size() - 1).getDOB().getYear() + " " + members.get(members.size() - 1).getDOB().getMonthValue() + " " + members.get(members.size() - 1).getDOB().getDayOfMonth() + " " + members.get(members.size() - 1).getAddress() + " " + members.get(members.size() - 1).getDiscipline() + " " + members.get(members.size() - 1).getType() + " " + members.get(members.size() - 1).getActivityLevel() + " " + members.get(members.size() - 1).getRegDate().getYear() + " " + members.get(members.size() - 1).getRegDate().getMonthValue() + " " + members.get(members.size() - 1).getRegDate().getDayOfMonth() + " " + members.get(members.size() - 1).getValidity());
          }catch (Exception e){
-            save.print(members.get(0).getID() + " " + members.get(0).getName() + " " + members.get(0).getDOB().getYear() + " " + members.get(0).getDOB().getMonthValue() + " " + members.get(0).getDOB().getDayOfMonth() + " " + members.get(0).getAddress() + " " + members.get(0).getDiscipline() + " " + members.get(0).getType() + " " + members.get(0).getActivityLevel());
+            save.print(members.get(0).getID() + " " + members.get(0).getName() + " " + members.get(0).getDOB().getYear() + " " + members.get(0).getDOB().getMonthValue() + " " + members.get(0).getDOB().getDayOfMonth() + " " + members.get(0).getAddress() + " " + members.get(0).getDiscipline() + " " + members.get(0).getType() + " " + members.get(0).getActivityLevel() + " " + members.get(0).getRegDate().getYear() + " " + members.get(0).getRegDate().getMonthValue() + " " + members.get(0).getRegDate().getDayOfMonth() + " " + members.get(0).getValidity());
          }
       }catch (Exception e){}
    }
